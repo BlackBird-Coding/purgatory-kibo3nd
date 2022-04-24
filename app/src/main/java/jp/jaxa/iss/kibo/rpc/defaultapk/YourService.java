bb652be204a5d6be4da42ddfaea52bd328207ae8
Thanadol Daroonsri <thanadol2005@gmail.com>
2022-04-25 00:59:14 +0700

    fix: moveToEuler

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -75,7 +75,6 @@ public class YourService extends KiboRpcService {
             Log.i("AR[count]:", String.valueOf(i));
             api.moveTo(point, quaternion, true);
             eu = DetectAR(api.getMatNavCam());
-            moveToEuler(10.71000, -7.70000, 4.48000,eu[0], eu[1], eu[2] );
             ++i;
         } while (i < 5 && eu == null);
 
@@ -111,12 +110,14 @@ public class YourService extends KiboRpcService {
             Aruco.detectMarkers(matImage, dictionary, corners, ids, parameter);
             List<Integer> values = flatten(ids);
             //for(int i = 0;i<values.size();i++) {
-                Aruco.estimatePoseSingleMarkers(corners, 0.05f, camMat, distCoeffs, rvecs, tvecs);
+            Aruco.estimatePoseSingleMarkers(corners, 0.05f, camMat, distCoeffs, rvecs, tvecs);
             //}
 
             List<float[]> arucos = new ArrayList<>();
 
-            for (int i = 0; i < rvecs.rows(); ++i) {
+
+            for(int i = 0; i < ids.size(); i++)
+            {
                 Mat rotationMatrix = new Mat();
                 Mat r = new Mat();
                 Mat q = new Mat();
@@ -131,17 +132,15 @@ public class YourService extends KiboRpcService {
             }
 
             float[] eu = new float[]{};
-            float pitch ;
 
-
-
+            /*
             for(int i=0;i<arucos.size();i++){
                 float[] aruco = arucos.get(i);
                 Log.i("AR[status]:",Arrays.toString(aruco));
                 if(aruco[0] == 4){
                     eu = new float[]{aruco[1],aruco[2],aruco[3]};
                 }
-            }
+            }*/
 
 
             return eu;
@@ -162,43 +161,36 @@ public class YourService extends KiboRpcService {
         return values;
     }
 
-    public void moveToEuler(double x_org, double y_org, double z_org, double x_des, double y_des, double z_des) {
-        double dx = x_des - x_org;
-        double dy = y_des - y_org;
-        double dz = z_des - z_org;
-        double magnitude = Math.sqrt((dx * dx) + (dy * dy) + (dz * dz));
-        double x_unit = dx / magnitude;
-        double y_unit = dy / magnitude;
-        double z_unit = dz / magnitude;
+    public void moveToEuler(double pos_x, double pos_y, double pos_z, double eu_x, double eu_y, double eu_z) {
+        double yaw=Math.toRadians(eu_x);
+        double pitch=Math.toRadians(eu_y);
+        double roll=Math.toRadians(eu_z);
 
-        double[][] matrix =
-                {
-                        {1, 0, 0},
-                        {x_unit, y_unit, z_unit}
-                };
+        double cosYaw = Math.cos(yaw / 2);
+        double cosPitch = Math.cos(pitch / 2);
+        double cosRoll = Math.cos(roll/2);
 
-        double x = matrix[0][1] * matrix[1][2] - matrix[1][1] * matrix[0][2];
-        double y = matrix[0][2] * matrix[1][0] - matrix[1][2] * matrix[0][0];
-        double z = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
-        double i = matrix[1][0] - matrix[0][0];
-        double j = matrix[1][1] - matrix[0][1];
-        double k = matrix[1][2] - matrix[0][2];
-        double q = Math.sqrt(x * x + y * y + z * z);
-        double p = Math.sqrt(i * i + j * j + k * k);
-        double theta = Math.acos((2 - p * p) / 2);
+        double sinYaw = Math.sin(yaw / 2);
+        double sinPitch = Math.sin(pitch / 2);
+        double sinRoll = Math.sin(roll/2);
 
-        double a = Math.sin(theta / 2) * x / q;
-        double b = Math.sin(theta / 2) * y / q;
-        double c = Math.sin(theta / 2) * z / q;
-        double w = Math.cos(theta / 2);
 
-        double pitch = -Math.atan((2 * (a * w + b * c)) / (w * w - a * a - b * b + c * c));
-        double roll = -Math.asin(2 * (a * c - b * w));
-        double yaw = Math.atan((2 * (c * w + a * b)) / (w * w + a * a - b * b - c * c));
-        double sx = (0.103 * Math.cos(roll + 0.279) / Math.cos(1.57080 + yaw));
-        double sy = (0.103 * Math.sin(roll + 0.279) / Math.cos(pitch));
+        double qua_x sinYaw * sinPitch * cosRoll - cosYaw * cosPitch * sinRoll;
+        double qua_y = sinYaw * cosPitch * cosRoll - cosYaw * sinPitch * sinRoll;
+        double qua_z = cosYaw * sinPitch * cosRoll - cosYaw * sinPitch * sinRoll;
+        double qua_w = cosYaw * cosPitch * cosRoll - sinYaw * sinPitch * sinRoll;
 
-        moveToWrapper((float) x_org - (float) sx, (float) y_org, (float) z_org + (float) sy, (float) a, (float) b, (float) c, (float) w);
+        final int LOOP_MAX = 5;
+        final Point point = new Point(pos_x, pos_y, pos_z);
+        final Quaternion quaternion = new Quaternion((float) qua_x, (float) qua_y, (float) qua_z, (float) qua_w);
+
+        int i = 0;
+        Result result;
+
+        do {
+            result = api.moveTo(point, quaternion, true);
+            ++i;
+        } while (!result.hasSucceeded() && i < LOOP_MAX);
     }
 
 }
