d54035cfec28c3532506344797634b02fafa24a7
Thanadol Daroonsri <thanadol2005@gmail.com>
2022-05-22 19:15:34 +0700

    stable

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -29,22 +29,10 @@ public class YourService extends KiboRpcService {
     protected void runPlan1(){
         Log.i("Bla_ckB","start");
         api.startMission();
-        moveToWrapper(10.71000, -7.70000, 4.58000,0, 0.707, 0, 0.707);
-        moveToWrapper(10.71000, -7.70000, 4.48000,0, 0.707, 0, 0.707);
+        moveToWrapper(10.71000, -7.70000, 4.53000,0, 0.707, 0, 0.707);
         api.reportPoint1Arrival();
 
-      /*  api.saveMatImage(api.getMatNavCam(),"0.png");
-        moveToWrapper(10.81000, -7.70000, 4.48000,0, 0.707, 0, 0.707);
-        api.saveMatImage(api.getMatNavCam(),"x10.png");
-        moveToWrapper(10.91000, -7.70000, 4.48000,0, 0.707, 0, 0.707);
-        api.saveMatImage(api.getMatNavCam(),"x20.png");
-        moveToWrapper(10.71000, -7.80000, 4.48000,0, 0.707, 0, 0.707);
-        api.saveMatImage(api.getMatNavCam(),"y10.png");
-        moveToWrapper(10.71000, -7.90000, 4.48000,0, 0.707, 0, 0.707);
-        api.saveMatImage(api.getMatNavCam(),"y20.png");
-    */
-       moveToTarget1(10.71000, -7.70000, 4.48000,0, 0.707, 0, 0.707);
-
+        moveToTarget1(10.71000, -7.70000, 4.48000,0, 0.707, 0, 0.707);
 
         moveToWrapper(11.3,-8.0000,4.4,0,0,-0.707, 0.707);
         moveToWrapper(11.3,-9.5000,4.4,0,0,-0.707, 0.707);
@@ -52,6 +40,12 @@ public class YourService extends KiboRpcService {
 
         //x-0.07 z+0.17
         moveToTarget2(11.27460, -9.92284, 5.29881,0, 0, -0.707, 0.707);
+
+        moveToWrapper(11.27460, -9.7, 5.29881,0, 0, -0.707, 0.707);
+        moveToWrapper(11.3,-9.5000,4.4,0,0,-0.707, 0.707);
+        moveToWrapper(11.3,-8.0000,4.4,0,0,-0.707, 0.707);
+
+        moveToWrapper(11.27460, -7.89178, 4.96538,0, 0, -0.707, 0.707);
 
         api.reportMissionCompletion();
     }
@@ -85,16 +79,17 @@ public class YourService extends KiboRpcService {
     private  void  moveToTarget1(double pos_x, double pos_y, double pos_z, double qua_x, double qua_y, double qua_z,
                                  double qua_w){
         double[] result;
-        moveToWrapper(pos_x,pos_y, pos_z+0.1, qua_x,qua_y,qua_z, qua_w);
         moveToWrapper(pos_x,pos_y, pos_z, qua_x,qua_y,qua_z, qua_w);
-        wait(2000);
+        wait(1000);
+        //api.flashlightControlFront(0.5f);
         result = DetectAR1(api.getMatNavCam());
         moveToWrapper(pos_x+result[0],pos_y+result[1], pos_z, qua_x,qua_y,qua_z, qua_w);
         wait(1000);
         api.laserControl(true);
         api.takeTarget1Snapshot();
-        api.saveMatImage(api.getMatNavCam(),"target1.png");
+        //api.saveMatImage(api.getMatNavCam(),"target1.png");
         api.laserControl(false);
+        return;
     }
 
     private  void  moveToTarget2(double pos_x, double pos_y, double pos_z, double qua_x, double qua_y, double qua_z,
@@ -102,14 +97,15 @@ public class YourService extends KiboRpcService {
         double[] result;
         moveToWrapper(pos_x,pos_y, pos_z, qua_x,qua_y,qua_z, qua_w);
         wait(1000);
+        api.flashlightControlFront(0.5f);
         result = DetectAR2(api.getMatNavCam());
-        moveToWrapper(pos_x,pos_y+0.1, pos_z+result[1], qua_x,qua_y,qua_z, qua_w);
         moveToWrapper(pos_x+result[0],pos_y, pos_z+result[1], qua_x,qua_y,qua_z, qua_w);
         wait(1000);
         api.laserControl(true);
         api.takeTarget2Snapshot();
         api.saveMatImage(api.getMatNavCam(),"target2.png");
         api.laserControl(false);
+        return;
     }
 
 
@@ -165,15 +161,15 @@ public class YourService extends KiboRpcService {
             double height = image.size().height;
             Imgproc.circle(image , new org.opencv.core.Point(width/2,height/2), 2, new Scalar(0, 100, 100), 3, 8, 0);
 
-            api.saveMatImage(image,"debug1.png");
+            //api.saveMatImage(image,"debug1.png");
 
             double newY = ((width/2 - centerX/16)/800)-0.031;
             double newX = ((height/2 - centerY/16)/800)-0.05;
 
-            Log.i("cX",String.valueOf(centerX/16));
+           /*  Log.i("cX",String.valueOf(centerX/16));
             Log.i("cY",String.valueOf(centerY/16));
             Log.i("newX", String.valueOf(newX));
-            Log.i("newY", String.valueOf(newY));
+            Log.i("newY", String.valueOf(newY));*/
 
             Log.i("AR[status]:", " end");
             return new double[]{newX,newY};
@@ -184,7 +180,7 @@ public class YourService extends KiboRpcService {
     }
 
 
-    private double[] DetectAR2(Mat matImage) {
+    private double[] DetectAR2O(Mat matImage) {
         Log.i("AR[status]:", " start");
         long startTime = SystemClock.elapsedRealtime();
 
@@ -252,6 +248,45 @@ public class YourService extends KiboRpcService {
             Log.i("AR[status]:", " Not detected");
         }
         return null;
+    }
+
+
+    private double[] DetectAR2(Mat matImage){
+        Mat image = new Mat();
+        double[][] camIntrinsics = api.getNavCamIntrinsics();
+        Mat camMat = new Mat(3, 3, CvType.CV_32FC1);
+        Mat distCoeffs = new Mat(1, 5, CvType.CV_32FC1);
+
+        camMat.put(0,0,camIntrinsics[0]);
+        distCoeffs.put(0,0,camIntrinsics[1]);
+
+        Imgproc.undistort(matImage,image,camMat,distCoeffs);
+
+        Imgproc.medianBlur(image, image,3);
+
+        Imgproc.adaptiveThreshold(image, image, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 23, 80);
+        api.saveMatImage(image,"th.png");
+        Mat circles = new Mat();
+        Imgproc.HoughCircles(image, circles, Imgproc.HOUGH_GRADIENT, 1.0,1000.0, 100.0, 30.0, 30, 55);
+        Log.i("Circle col", String.valueOf(circles.cols()));
+
+        org.opencv.core.Point center = new org.opencv.core.Point();
+
+            double[] c = circles.get(0, 0);
+            center = new org.opencv.core.Point(Math.round(c[0]), Math.round(c[1]));
+            // circle center
+           /* Imgproc.circle(image, center, 1, new Scalar(0, 200, 100), 3, 8, 0);
+            // circle outline
+            int radius = (int) Math.round(c[2]);
+            Imgproc.circle(image, center, radius, new Scalar(80, 0, 255), 3, 8, 0);
+            Log.i("Circle x", String.valueOf(center.x));
+            Log.i("Circle y", String.valueOf(center.y));*/
+        api.saveMatImage(image,"debug2.png");
+        double newX = -((735 - center.x)/950);//-0.031;
+        double newZ = (-(460 - center.y)/950)+0.03;//+0.02;
+        Log.i("new X",String.valueOf(newX));
+        Log.i("new Z",String.valueOf(newZ));
+        return new double[]{newX,newZ};
     }
 
     private void wait(int ms){
